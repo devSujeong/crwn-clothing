dfc35dd99096429566329302b0480a9f03b199ba
crystal - Kim Sujeong <sjkim@2ndsyndrome.com>
2021-01-29 17:30:49 +0900

    implemented firebase utils, included ability to store authenticated user~

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,13 +12,42 @@ const config = {
   measurementId: "G-NGG8QGJHJ3"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
-export const firesotre = firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+// firestore.collection('users').doc('11VopWlFRJuuv4GVzpzn').collection('cartItems').doc('GOrKnOeqHyQZ0dlcM0Kr');
+// firestore.doc('/users/11VopWlFRJuuv4GVzpzn');
+// firestore.collection('/users/11VopWlFRJuuv4GVzpzn');
 export default firebase;
